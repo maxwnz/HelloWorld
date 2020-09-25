ab515449506bd6faafb98b07350323af185f8ef6
MaxWenz <MaxWenz@0-208-12>
2020-09-25 12:00:04 +0200

    FizzBuzz

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,21 @@
-
 public class HelloWorld {
 
+	static int counter = 1;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		
+		
+		while(counter<=100) {
+			if(counter%3==0 && counter%5==0)
+				System.out.println("FizzBuzz");
+			else {
+			 if(counter%5==0) {System.out.println("FizzBuzz");}
+			  else 
+				if(counter%3==0){System.out.println("Buzz");}
+			     else {System.out.println(counter);}
+			  }
+           counter++;
+		}
 	}
 
 }
